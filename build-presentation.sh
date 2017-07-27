bbf70d9d637ffd669c8bfab90ecba64014483bc2
ridge731 <ridge731@gmail.com>
2017-07-27 12:10:22 -0400

    added text to build-presentation.sh

--- build-presentation.sh
+++ build-presentation.sh
@@ -6,3 +6,10 @@ TRANSITION=slide
 THEME=black
 
 pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
+
+# Final assignment will be a presentation consisting of audio accompanied by images/gifs.
+
+
+
+
+
