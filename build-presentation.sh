2d80db02263454a3eb6a82e00b643359804a5537
ridge731 <ridge731@gmail.com>
2017-07-29 22:51:12 -0400

    Converted file to html

--- build-presentation.sh
+++ build-presentation.sh
@@ -5,7 +5,7 @@ TEMPLATE=template-index.html
 TRANSITION=slide
 THEME=black
 
-pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
+pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="https://ridge-hamre.github.io/presentation/" PresentationSlides.md -o PresentationIndex.html
 
 # Final assignment will be a presentation consisting of audio accompanied by images/gifs.
 
